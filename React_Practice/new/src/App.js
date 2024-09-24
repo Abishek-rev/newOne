5c6a0250ac0b32a9acf193c4df21dc3a500416c6
Abishek-personal <abishek4691@gmail.com>
2024-09-24 21:37:17 +0530

    created component in tanstack

--- React_Practice/new/src/App.js
+++ React_Practice/new/src/App.js
@@ -3,15 +3,14 @@ import "./App.css";
 import InputField from "./InputField";
 import { useRef, useState } from "react";
 import Asyncdatas from "./components/Asyncdatas";
+import Home from "./components/Tanstack/components/Home";
 // import { QueryClient } from "@tanstack/react-query";
 // import AnimationAndTransition from "./components/css_Conepts/AnimationAndTransition";
 function App() {
   return (
     
       <div className="App">
-        <Asyncdatas />
-        {/* <AnimationAndTransition /> */}
-        {/* <InputField/> */}
+        <Home/>
       </div>
   );
 }
